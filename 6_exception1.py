df105adf6aac0443da3cbf676a7fc075ee5f5d8a
Vladimir <vpokutniy@inbox.ru>
2020-09-10 02:00:12 +0300

    work done

--- 6_exception1.py
+++ 6_exception1.py
@@ -4,17 +4,25 @@
 
 Исключения: KeyboardInterrupt
 
-* Перепишите функцию ask_user() из задания while2, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
+* Перепишите функцию ask_user() из задания while2, чтобы она
+  перехватывала KeyboardInterrupt, писала пользователю "Пока!"
   и завершала работу при помощи оператора break
-    
+
 """
 
 def ask_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+
+    dct = dict([('Как дела?', 'Хорошо!'), ('Что делаешь?', 'Программирую'), ('Когда ложишься спать?', 'Рано'),
+                ('Ты поел?', 'да')])
+    # inp=input("Введите вопрос")
+    while True:
+        try:
+            inp = input(" Введите вопрос")
+            if inp in dct:
+                print(dct.get(inp))
+                break
+        except KeyboardInterrupt:
+            print(' Пока')
+            break
 if __name__ == "__main__":
     ask_user()
--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -1,18 +1,21 @@
+
 """
 Домашнее задание №1
-
 Использование библиотек: ephem
-
 * Установите модуль ephem
-* Добавьте в бота команду /planet, которая будет принимать на вход 
+* Добавьте в бота команду /planet, которая будет принимать на вход
   название планеты на английском, например /planet Mars
-* В функции-обработчике команды из update.message.text получите 
+* В функции-обработчике команды из update.message.text получите
   название планеты (подсказка: используйте .split())
-* При помощи условного оператора if и ephem.constellation научите 
+* При помощи условного оператора if и ephem.constellation научите
   бота отвечать, в каком созвездии сегодня находится планета.
-
 """
 import logging
+import ephem
+# import datetime
+from datetime import datetime
+today = datetime.today()
+currentYear = datetime.now().year
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -25,34 +28,49 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 PROXY = {
     'proxy_url': 'socks5://t1.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {
-        'username': 'learn', 
+        'username': 'learn',
         'password': 'python'
     }
 }
 
 
-def greet_user(bot, update):
+def greet_user(update, context):
     text = 'Вызван /start'
     print(text)
     update.message.reply_text(text)
 
 
-def talk_to_me(bot, update):
-    user_text = update.message.text 
+def talk_to_me(update, context):
+    user_text = update.message.text
     print(user_text)
     update.message.reply_text(user_text)
- 
+
+def planet(update, context):
+    text="Добавьте название планеты на английском"
+    update.message.reply_text(text)
+
+
+def planet_select(update, context):
+    user_text = update.message.text
+    if user_text =='Mars':
+        pl=ephem.Mars(currentYear)
+        constel=ephem.constellation(pl)
+        update.message.reply_text(constel)
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
-    
+    mybot = Updater("1397084816:AAFWzpPtbPawQrxl1meF9GkRKZUu0yz2XGU", request_kwargs=PROXY,use_context=True)
+
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    
+    # dp.add_handler(CommandHandler("start", greet_user))
+    # dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    dp.add_handler(CommandHandler("planet", planet))
+    dp.add_handler(MessageHandler(Filters.text, planet_select))
+
+
     mybot.start_polling()
     mybot.idle()
-       
+
 
 if __name__ == "__main__":
     main()
